09d92374f5ba88a878e31988ecdb4a224a20df64
Paul <paultowers2005@yahoo.com.au>
2019-12-05 20:41:31 +1100

    Created contact form component

---
+++ client/src/components/contacts/ContactForm.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+
+const ContactForm = () => {
+  const [contact, setContact] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    type: "personal"
+  });
+
+  const { name, email, phone, type } = contact;
+
+  //Target specific item to change
+
+  const onChange = e =>
+    setContact({
+      ...contact,
+      [e.target.name]: e.target.value
+    });
+
+  return (
+    <form>
+      <h2 className="text-primary">Add Contact</h2>
+      <input
+        type="text"
+        placeholder="Name"
+        name="name"
+        value={name}
+        onChange={onChange}
+      ></input>
+      <input
+        type="text"
+        placeholder="Email"
+        name="email"
+        value={email}
+        onChange={onChange}
+      ></input>
+      <input
+        type="text"
+        placeholder="Phone"
+        name="phone"
+        value={phone}
+        onChange={onChange}
+      ></input>
+      <h5>Contact Type</h5>
+      <label>
+        Personal
+        <input
+          type="radio"
+          name="type"
+          value="personal"
+          checked={type === "personal"}
+        ></input>
+      </label>
+      <label>
+        Professional
+        <input
+          type="radio"
+          name="type"
+          value="professional"
+          checked={type === "professional"}
+        ></input>
+      </label>
+      <div>
+        <input
+          type="submit"
+          value="Add Contact"
+          className="btn btn-primary btn-block"
+        ></input>
+      </div>
+    </form>
+  );
+};
+
+export default ContactForm;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const connectDB = require('./config/db')
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -7,24 +7,23 @@ const app = express();
 connectDB();
 
 //init Middleware
-app.use(express.json({
-    extended: false
-}));
+app.use(
+    express.json({
+        extended: false
+    })
+);
 
-app.get('/', (req, res) => res.json({
-    msg: 'Welcome to the contact keeper API'
-}));
+app.get("/", (req, res) =>
+    res.json({
+        msg: "Welcome to the contact keeper API"
+    })
+);
 
 // Define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
-
-
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
-    PORT,
-    () => console.log(`Server started on port ${PORT}`)
-)
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
