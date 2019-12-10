4e0fd795d364dff5bfee9316524eefb343fda442
Paul <paultowers2005@yahoo.com.au>
2019-12-10 20:59:14 +1100

    created login and register form, started alert context

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,8 @@ import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
-
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 import "./App.css";
 
 const App = () => {
@@ -19,6 +20,8 @@ const App = () => {
               <Switch>
                 <Route exact path="/" component={Home} />{" "}
                 <Route exact path="/about" component={About} />{" "}
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
               </Switch>{" "}
             </div>
           </Fragment>{" "}
