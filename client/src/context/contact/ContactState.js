b6b3e94b791c79ccfaead7b1be634e36b3802d74
Paul <paultowers2005@yahoo.com.au>
2019-12-11 12:14:28 +1100

    connected update contact functionality to backend

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -72,6 +72,26 @@ const ContactState = props => {
     }
   };
 
+  //Update Contact
+
+  const updateContact = async contact => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+      dispatch({ type: UPDATE_CONTACT, payload: res.data });
+    } catch (error) {
+      dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
+    }
+  };
+
   //clear Contacts
 
   const clearContacts = () => {
@@ -88,12 +108,6 @@ const ContactState = props => {
 
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
-  };
-
-  //Update Contact
-
-  const updateContact = contact => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
 
   //Filter contacts
